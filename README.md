f66a391792795cda16c909bc60d08333461fa808
Gabi Volpe <volpegabriel@gmail.com>
2015-08-05 14:28:05 +0100

    Avoid to create a new file before using the sed command.

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ bash-scripting
 
 Generic Bash Scripting & Utilities that I've been creating for repetitive tasks.
 
-* Search and Replace data in a file.
+* Search and replace data in a file.
 * Run same command for every enviroment. AKA run same command in a loop.
--- replace-data-in-file.sh
+++ replace-data-in-file.sh
@@ -9,14 +9,16 @@ else
 	echo "File to search and replace: " $1
 	echo ""
 
-	# Creating new file
+	# Creating new file name
         new_file="NEW-"$1
-	cp $1 $new_file
 
-	# Search for $2 and replace for $3 in line in $new_file (using variables $2 and $3)
-	sed -i "s@$2@$3@" $new_file
+	# Search for $2 and replace for $3 in $1 and generate a $new_file (using variables $2 and $3)
+        sed "s@$2@$3@" $1 >> $new_file
 
-	# Search and replace using values
+	# Search for $2 and replace for $3 in line in the same file (using variables $2 and $3)
+	# sed -i "s@$2@$3@" $1
+
+	# Search and replace using flat values
 	# sed 's/value-to-search-for/replacement-value/g' $file-to-search-for >> $new_file
 
 	# Granting execution permissions
