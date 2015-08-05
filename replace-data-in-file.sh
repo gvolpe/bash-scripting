5e5d0ea73aa79084e95a6b8bb732554a4b40f6cb
Gabi Volpe <volpegabriel@gmail.com>
2015-08-05 14:16:52 +0100

    Fixed use of the sed command when using variables instead of flat values.

--- replace-data-in-file.sh
+++ replace-data-in-file.sh
@@ -9,9 +9,15 @@ else
 	echo "File to search and replace: " $1
 	echo ""
 
-	# Search for $2 and replace for $3 in file $1
-	new_file="NEW-"$1
-	sed 's/$2/$3/g' $1 >> $new_file
+	# Creating new file
+        new_file="NEW-"$1
+	cp $1 $new_file
+
+	# Search for $2 and replace for $3 in line in $new_file (using variables $2 and $3)
+	sed -i "s@$2@$3@" $new_file
+
+	# Search and replace using values
+	# sed 's/value-to-search-for/replacement-value/g' $file-to-search-for >> $new_file
 
 	# Granting execution permissions
 	sudo chmod a+x $new_file
